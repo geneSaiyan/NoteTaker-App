efe6293f88f908b088f91eb773300a54567aefad
Gene Edwards <eedwards@su.edu>
2020-02-01 13:04:38 -0500

    Added dependencies to server.js file

--- server.js
+++ server.js
@@ -5,37 +5,37 @@ var fs = require("fs");
 
 // Set up for use of the Express App
 var app = express();
-var PORT = 3000;
+var PORT = 8080;
 
 // Data parsing using express app
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Routes
-// =============================================================
+// Routes section-----------------------------------------------
 
+// Notes route using notes.html
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
+// API Notes route
 app.get("/api/notes", function (req, res) {
-    let jsonContent = getJSONData();
+    var jsonContent = getJSONData();
     return res.json(jsonContent);
     // res.sendFile(path.join(__dirname, "db", "db.json"));
 });
 
+// Default to the index.html page
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Create New Note - takes in JSON input
+// Create New Note a new note using fs and JSON
 app.post("/api/notes", function (req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    let newNote = req.body;
+    var newNote = req.body;
     console.log(newNote);
-    let jsonContent = getJSONData();
+    var jsonContent = getNoteJSON();
     jsonContent.push(newNote);
     fs.writeFileSync(path.join(__dirname, "db", "db.json"), JSON.stringify(jsonContent));
     res.json(jsonContent);
@@ -45,7 +45,7 @@ app.post("/api/notes", function (req, res) {
 app.delete("/api/notes/:id",
     function (req, res) {
         console.log("Deleting id:" + req.params.id);
-        let jsonContent = getJSONData();
+        let jsonContent = getNoteJSON();
         let updatedJSON = jsonContent.filter(function (definition) {
             return definition.id.toLowerCase() !== req.params.id.toLowerCase();
         });
@@ -53,7 +53,7 @@ app.delete("/api/notes/:id",
         res.json(updatedJSON); //sending the updated response back to client app.
     });
 
-function getJSONData() {
+function getNoteJSON() {
     let contents = fs.readFileSync(path.join(__dirname, "db", "db.json"));
     return JSON.parse(contents);
 }
